3f9b3b96868e8b1b86cde5d43889e7209c199665
Jaani <jaanifrancis@icloud.com>
2026-04-09 13:00:20 +0800

    chore: switch CI and backend region to ap-southeast-1

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
 	backend "s3" {
 		bucket         = "portfolio-builder-terraform-state-353695642481"
 		key            = "prod/terraform.tfstate"
-		region         = "ap-southeast-2"
+		region         = "ap-southeast-1"
 		encrypt        = true
 		dynamodb_table = "terraform-locks"
 	}
